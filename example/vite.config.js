addb44b21d918070a050d43614cd50d3a7552540
飞天御剑流co <521luqiya@gmail.com>
2024-08-22 13:47:36 +0800

    feat: add vite https config

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,8 +1,15 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import fs from "fs";
 
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync("./127.0.0.2+1-key.pem"),
+      cert: fs.readFileSync("./127.0.0.2+1.pem"),
+    },
+  },
   build: {
     rollupOptions: {
       input: {
